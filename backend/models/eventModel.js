f9b3b83344491c10d8ce74d8e70aeab0744f0e2e
IT22602114 <it22602114@my.sliit.lk>
2025-02-03 12:23:42 +0530

    create event model

---
+++ backend/models/eventModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+const speakerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    }
+})
+
+const eventSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    currency: {
+        type: String,
+        required: true
+    },
+    speakers: [speakerSchema],
+    image: {
+        type: String,
+        required: true
+    },
+    platform: {
+        type: String,
+    },
+    time: {
+        type: String,
+    }
+})
+
+const Event = mongoose.model('Event', eventSchema)
+
+module.exports = Event
