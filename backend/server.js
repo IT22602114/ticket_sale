06a339a6e8f0046934d5a8b04764869d09ee16a3
IT22602114 <it22602114@my.sliit.lk>
2025-02-03 09:48:37 +0530

    add a middleware and a function to handle errors

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,6 @@ const app = express()
 
 app.use(express.json())
 
-app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
-
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(process.env.PORT, () => {
@@ -21,3 +16,13 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((error) => {
         console.log(error)
     })
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal Server Error'
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
+})
